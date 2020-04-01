ed380152d243018ce5f0806b69a0ee4dbaed9b31
Chin Kuay <kuaychin@gmail.com>
2020-04-01 15:35:44 -0500

    fixed missing semicolons

--- ans.sql
+++ ans.sql
@@ -22,49 +22,101 @@ VALUES
     ('Shawn Garcia', 33, 182, 'Chicago', 'white');
 
 --3
-SELECT * FROM person 
+SELECT *
+FROM person
 ORDER BY height DESC;
 
 --4
-SELECT * FROM person 
+SELECT *
+FROM person
 ORDER BY height ASC;
 
 --5
-SELECT * FROM person 
+SELECT *
+FROM person
 ORDER BY age DESC;
 
 --6
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE age >= 20;
 
 --7
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE age = 18;
 
 --8
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE age < 20 AND age > 30;
 
 --9
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE age != 27;
 
 --10
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE favorite_color != 'red';
 
 --11
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE favorite_color NOT IN ('red', 'blue');
 
 --12
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE favorite_color IN ('orange', 'green');
 
 --13
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE favorite_color IN ('orange', 'green', 'blue');
 
 --14
-SELECT * FROM person 
+SELECT *
+FROM person
 WHERE favorite_color IN ('yellow', 'purple');
+
+--------------------------------------------------
+
+--Table-orders
+--1
+CREATE TABLE orders
+(
+    order_id SERIAL PRIMARY KEY,
+    person_id INT NOT NULL,
+    product_name VARCHAR(255),
+    product_price INT NOT NULL,
+    quantity INT NOT NULL
+);
+
+--2
+INSERT INTO orders
+    (person_id ,product_name, product_price, quantity)
+VALUES
+    (0, 'Product 1', 5, 20),
+    (1, 'Product 2', 4, 1),
+    (2, 'Product 3', 40, 10),
+    (3, 'Product 4', 1, 200),
+    (4, 'Product 5', 10, 90);
+
+--3
+SELECT *
+FROM orders;
+
+--4
+SELECT sum(quantity)
+FROM orders;
+
+--5
+SELECT sum(product_price * quantity)
+FROM orders;
+
+--6
+SELECT sum(product_price*quantity)
+FROM orders
+WHERE person_id = 1;
